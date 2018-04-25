e4e4bc681822489bd5d5b8531fc634ce1c37baf7
felipe gutierrez <afgb1996@gmail.com>
2018-04-24 19:30:15 -0500

    Se crea la clase Estudiante.

---
+++ src/ordenamientoporarreglo/Estudiante.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordenamientoporarreglo;
+
+/**
+ *
+ * @author felipe_2
+ */
+public class Estudiante {
+    
+    private String nombre;
+    private String apellido;
+    private String identificacion;
+    private int puesto;
+    private double nota;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public void setPuesto(int puesto) {
+        this.puesto = puesto;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+
+    public String imprimir(){
+        return "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public int getPuesto() {
+        return puesto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+}
---
+++ src/ordenamientoporarreglo/OrdamientoObjetos.java
@@ -0,0 +1,18 @@
+package ordenamientoporarreglo;
+
+/**
+ *
+ * @author felipe_2
+ */
+public class OrdamientoObjetos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        OrdamientoObjetos [] arrayEstudiantes = new OrdamientoObjetos [5];
+    }
+    
+    
+}
